c0ca6f89ca61ce7223c38f614e6fd4477b8f08f0
Luis Urrutia <mcc.urrutia@gmail.com>
2022-02-25 16:29:52 +1000

    CRUD can be used in the React Apo

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import styles from "./styles.modules.css";
+import styles from "./styles.module.css";
 
 const Signup = () => {
 	const [data, setData] = useState({
